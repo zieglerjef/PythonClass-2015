0753adabe9d8591e9966986792e7d26840989c57
zieglerjef <zieglerjef@gmail.com>
2015-08-17 14:55:27 -0500

    Add project

--- Project/projectScraping.py
+++ Project/projectScraping.py
@@ -3,22 +3,25 @@ import urllib2
 import csv
 import re
 
-web_address='http://www.europarl.europa.eu/plenary/en/minutes.html#sidesForm' #identify initial website
+web_address='http://www.europarl.europa.eu/sides/getDoc.do?type=PV&reference=20140417&secondRef=TOC&language=EN' #identify initial website
 web_page = urllib2.urlopen(web_address)
 soup = BeautifulSoup(web_page.read()) #define soup
 
-agenda_section = soup.find_all('a',{'class':'title'}) #locate the div class that petition titles are located within and pull out the url of each petition
+table = soup.find('table',{'class':'doc_box_header'})
+for row in table.find_all('tr'):
+	agendaLinks = table.find_all('a')[1:]
 
 def clean(object): #create function to clean html
 	for link in object:
 		return re.sub(r'<[^>]+>', '', str(link))
 
-def getData(object): #create function to generate petition attributes from each minutes page (Title, Published Date, Content)
-	web_page = urllib2.urlopen(object) #update webpage to each individual instance's url
+def getData(items): #create function to generate petition attributes from each minutes page (Title, Published Date, Content)
+	web_page = urllib2.urlopen(items) #update webpage to each individual instance's url
 	soup2 = BeautifulSoup(web_page.read()) #redefine soup for each minutes' page
-	motion_type = soup2.find_all('p',{'class':'contents'}) #grab petition title
-	motion_date = soup2.find_all('title') #take published date
-	return {"Title":clean(petition_title), "Date":clean(petition_date), "Content":clean(petition_issues)}
+	sections = soup2.find_all('table',{'class':'doc_box_header'}) #grab petition title
+	agendaContent = sections.find_all('td')	
+	print agendaContent
+	return {"Date":clean(agendaDate)}
 
 with open('europeanParliamentMinutes.csv', 'wb') as f: #open csv file to write to
 	my_writer = csv.DictWriter(f, fieldnames=("Title", "Date", "Content")) #create dictionary w/ item names
